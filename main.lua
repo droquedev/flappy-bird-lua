f911f5156e9bffb8414cf1191f33734d8e7028e0
David Roque <davidroque21@hotmail.com>
2023-07-01 16:13:45 -0400

    add pipe rendering and destroy

--- main.lua
+++ main.lua
@@ -1,14 +1,6 @@
 Class = require 'class'
 local push = require 'push'
 
-require 'Bird'
-
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
-
-VIRTUAL_WIDTH = 512
-VIRTUAL_HEIGHT = 288
-
 local background = love.graphics.newImage('assets/imgs/background.png')
 local backgroundScroll = 0
 
@@ -16,15 +8,30 @@ local ground = love.graphics.newImage('assets/imgs/ground.png')
 local groundScroll = 0
 
 local BACKGROUND_SCROLL_SPEED = 30
-local GROUND_SCROLL_SPEED = 60
+GROUND_SCROLL_SPEED = 60
 
 local BACKGROUND_LOOPING_POINT = 413
 
+require 'Bird'
+require 'Pipe'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
 local bird = Bird()
+
+local pipes = {}
+
+local spawnTimer = 0
 
 function love.load()
   love.graphics.setDefaultFilter('nearest', 'nearest')
   love.window.setTitle("David's Flappy Bird")
+
+  math.randomseed(os.time())
 
   push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
     fullscreen = false,
@@ -39,7 +46,22 @@ function love.update(dt)
   backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
   groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
 
+  spawnTimer = spawnTimer + dt
+
+  if spawnTimer > 2 then
+    table.insert(pipes, Pipe())
+    spawnTimer = 0
+  end
+
   bird:update(dt)
+
+  for k, pipe in pairs(pipes) do
+    pipe:update(dt)
+    if pipe.x < -pipe.width then
+      table.remove(pipes, k)
+    end
+  end
+
   love.keyboard.keysPressed = {}
 end
 
@@ -62,6 +84,10 @@ function love.draw()
   push:start()
 
   love.graphics.draw(background, -backgroundScroll, 0)
+  for k, pipe in pairs(pipes) do
+    pipe:render()
+  end
+
   love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
 
   bird:render()
